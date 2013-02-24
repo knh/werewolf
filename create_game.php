5556a41783c75de3cd88da81e6008225bb230ca4
Dihong Gao <gao118@expert.ics.purdue.edu>
2013-02-24 13:14:50 -0500

    create game partially functioning. Next step: reset and clear

--- create_game.php
+++ create_game.php
@@ -1,11 +1,11 @@
 <?php
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
-$player_num=$_POST['player_num'];
 $raw_role_string=$_POST['role_list'];
 $role_array=preg_split("/(\r\n|\n|\r)/", $raw_role_string);
 $role_num_array=array();
 $counter=0;
+$player_num=0;
 foreach($role_array as &$role){
   $sub_role = explode("*", $role);
   if(isset($sub_role[1])){
@@ -15,14 +15,44 @@ foreach($role_array as &$role){
   else{
     $role_num_array[$counter] = 1;
   }
+  $player_num += $role_num_array[$counter];
   $counter++;
 }
 for($counter=0; $counter < count($role_array); $counter++){
   echo $role_array[$counter] . " * " . $role_num_array[$counter] ."</br>";
 }
-$link=mysql_connect('mydb.ics.purdue.edu:3306/gao118',
+$game_id=rand(100000, 999999);
+$mysqli=new mysqli("mydb.ics.purdue.edu", 
+                    "gao118", "polaris", "gao118", "3306");
+$query="SELECT * FROM werewolf_gameid WHERE game_id=" . $game_id;
+if ($mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+          exit();
+}
+while(true){
+  $result=$mysqli->query($query);
+  if($result->num_rows == 0)
+    break;
+  $game_id=rand(100000, 999999);
+  $query="SELECT * FROM werewolf_gameid WHERE game_id=" . $game_id;
+}
+echo "game id: " . $game_id;
+if($mysqli->query("INSERT INTO werewolf_gameid (game_id, player_num_limit)".                "VALUES ('" . $game_id . "', '" . $player_num ."')") == true){
+  echo "</br>Create game succeed. Your game id is " . $game_id ."</br>";
+}
+else{
+  echo "</br>Create game failed. Please try again later.</br>";
+}
+
+$query="INSERT INTO werewolf_detail (game_id, all_roles) VALUES ('" . $game_id . "', '" . $raw_role_string . "')";
+if($mysqli->query($query)){
+  echo "Detail writing success. </br>";
+}else{
+  echo "Detail writing failed. </br>";
+}
+/*$link=mysql_connect('mydb.ics.purdue.edu:3306/gao118',
                     'gao118', 'polaris')
                   or die("Can not connect to db. " . mysql_error());
 mysql_select_db('gao118') or die ('Can not select db.' . mysql_error());
-$result=mysql_query($query) or die ("Query failed. " . mysql_error());
+$result=mysql_query($query) or die ("Query failed. " . mysql_error()); */
 ?>
